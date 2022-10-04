3a7d4fc5c78687a1ad90fe1c8b2a4430b57c5ac7
Edgar Gonzalez <edgar_gm6@hotmail.com>
2022-10-03 20:27:41 -0500

    finished

---
+++ actividad-6/auth/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0iojLABePGDV_6Yy6ypooncrtm697-Lc",
+    authDomain: "optativavii.firebaseapp.com",
+    databaseURL: "https://optativavii-default-rtdb.firebaseio.com",
+    projectId: "optativavii",
+    storageBucket: "optativavii.appspot.com",
+    messagingSenderId: "794804461503",
+    appId: "1:794804461503:web:35a4e93afcfb186880cb0a"
+  };
